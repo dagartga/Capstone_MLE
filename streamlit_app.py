35741d3bc19fd8c666a1b780e321d7fb642e3bbe
Dagart Allison <dagartga@gmail.com>
2022-04-10 12:40:38 -0400

    update streamlit app to adjust date

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,20 @@ import streamlit as st
 import s3fs
 import os
 import pandas as pd
-
+from datetime import date, timedelta
 from model import prediction
+
+# run the prediction model
+# outputs the next day price prediction and the date of today's data
 next_day_pred, todays_date = prediction()
 
+# adjust the date for the next day for more readability of result
+pred_date = todays_date + timedelta(days=1)
+
 st.subheader('BTC Next Day Price Prediction')
-st.write(todays_date)
+st.subheader('Prediction Date')
+st.write(pred_date)
+st.subheader('Prediction Price (USD)')
 st.write(next_day_pred)
 
 
