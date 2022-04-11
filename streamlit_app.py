246ec82da60eefc256510fa65fecc8b9962aea8d
Dagart Allison <dagartga@gmail.com>
2022-04-11 19:58:31 -0400

    add traceback print to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,32 +6,38 @@ import os
 import pandas as pd
 from datetime import datetime, date, timedelta
 from model import prediction
+import traceback
 
+
+try:
 # run the prediction model
 # outputs the next day price prediction and the date of today's data
-next_day_pred, todays_date = prediction()
-today = datetime.strptime(todays_date, '%Y/%m/%d')
+    next_day_pred, todays_date = prediction()
+    today = datetime.strptime(todays_date, '%Y/%m/%d')
 
 # adjust the date for the next day for more readability of result
-pred_date = today + timedelta(days=1)
+    pred_date = today + timedelta(days=1)
 
 # convert to string format
-pred_date = pred_date.strftime('%m/%d/%Y')
+    pred_date = pred_date.strftime('%m/%d/%Y')
 
-st.subheader('BTC Next Day Price Prediction')
-st.subheader('Prediction Date')
-st.write(pred_date)
-st.subheader('Prediction Price (USD)')
-st.write(next_day_pred)
+    st.subheader('BTC Next Day Price Prediction')
+    st.subheader('Prediction Date')
+    st.write(pred_date)
+    st.subheader('Prediction Price (USD)')
+    st.write(next_day_pred)
 
 
 
 # Retrieve file contents
-s3 = s3fs.S3FileSystem(anon=False)
+    s3 = s3fs.S3FileSystem(anon=False)
 
-with s3.open('btcpricedata/Merged_Unconverted_BTC_Data.csv', 'rb') as f:
-    df = pd.read_csv(f)
+    with s3.open('btcpricedata/Merged_Unconverted_BTC_Data.csv', 'rb') as f:
+        df = pd.read_csv(f)
 
 # Print results
-st.subheader('Raw Data')
-st.write(df)
+    st.subheader('Raw Data')
+    st.write(df)
+
+except Exception as e:
+    st.write(e)
